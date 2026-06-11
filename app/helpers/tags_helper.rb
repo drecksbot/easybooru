8c839d57ea1aa8bff2970d7f775e0e493aa0c470
drecksbot <drecksbxt@gmail.com>
2026-06-12 00:42:51 +0200

    Refactor quick tag category button and form styles for consistency

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 module TagsHelper
+  QUICK_TAG_CATEGORY_BUTTON_STYLE = "display: inline; border: 0; padding: 0; background: transparent; color: var(--link-color); font: inherit; line-height: inherit; cursor: pointer; box-shadow: none;".freeze
+  QUICK_TAG_CATEGORY_FORM_STYLE = "display: inline; margin: 0; padding: 0;".freeze
+
   QUICK_TAG_CATEGORY_LINKS = {
     "g" => TagCategory::GENERAL,
     "a" => TagCategory::ARTIST,
@@ -18,7 +21,9 @@ module TagsHelper
     current_letter = QUICK_TAG_CATEGORY_LINKS.key(tag.category)
     links = QUICK_TAG_CATEGORY_LINKS.except(current_letter).map do |letter, category|
       button_to letter, tag_path(tag), method: :put, class: "wiki-link tag-category-shortcut-link",
-        form_class: "tag-category-shortcut-form", title: "Set category to #{TagCategory.reverse_mapping[category].capitalize}", params: {
+        form: { class: "tag-category-shortcut-form", style: QUICK_TAG_CATEGORY_FORM_STYLE },
+        style: QUICK_TAG_CATEGORY_BUTTON_STYLE,
+        title: "Set category to #{TagCategory.reverse_mapping[category].capitalize}", params: {
         tag: { category: category },
         quick_category: true,
         url: request.fullpath,
